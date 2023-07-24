83c504c2ba0a23388923800ec83274712cc846b8
graciano1997 <gracianomanuelhenrique@gmail.com>
2023-07-24 09:39:51 +0100

    hard reset

--- src/module/tasksToDo.js
+++
@@ -1,5 +0,0 @@
-export default class TasksToDo {
-  constructor() {
-    this.taskArray = (JSON.parse(localStorage.getItem('taskDB')) !== null) ? (JSON.parse(localStorage.getItem('taskDB'))) : ([]);
-  }
-}
